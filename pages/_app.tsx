2b8dd79a712714bf947e30e1e3f01d169fbff054
Koakoalla <104795085+Koakoalla@users.noreply.github.com>
2022-12-05 23:35:58 +0300

    basic settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, { Fragment } from "react"
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>UGC-аналитика</title>
+      <link ref="icon" href="/favicom.ico"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
+export default App;
